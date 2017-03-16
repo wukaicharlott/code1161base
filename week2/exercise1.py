8fb21cb52d0dd6cf5f6eed6b662109b6f65630df
Ben <ben@notionparallax.co.uk>
2017-03-17 10:52:26 +1100

    AntaBaka

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -8,14 +8,18 @@ Make sure that your code still runs without any errors by pressing
 from __future__ import division
 from __future__ import print_function
 print  ("hello! Let's get started")
-jobs=['get','this',
-'file','to','pass',
-  'the','linter']
-InOtherWords="make it show no linter errors"
+jobs = ['get', 'zthis',
+        'file', 'to', 'pass',
+        'the', 'linter']
+InOtherWords = "make it show no linter errors"
 import os
 print(jobs)
 print(InOtherWords)
-print(1+1,"is smaller than",7*0.5,"is",(1+1)<(7*0.5),", which is a relief!")
-def usefulFunction () :
+print(1+1, "is smaller than", 7*0.5, "is", (1+1) < (7*0.5),
+      " , which is a relief!")
+
+
+def usefulFunction():
+    """print 'WTF'
     print(os.getcwd())
-usefulFunction( )
+usefulFunction()
